35c4670692fd5d79814f89efe19e78633935a868
Sophia <suna9267@gmail.com>
2021-04-22 14:06:13 +0900

    회원가입 페이지 컨트롤러, 뷰 수정

--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -8,13 +8,14 @@ class View {
     this.phone = document.getElementById('input-phone');
     this.email = document.getElementById('input-email');
     this.submit = document.getElementById('submit-btn');
-    console.log(this.id, this.pw, this.checkPw, this.name, this.birth, this.phone, this.email);
+    
+    // console.log(this.id, this.pw, this.checkPw, this.name, this.birth, this.phone, this.email);
   }
 
-  signUp() {
-    this.submit.addEventListener("click", () => {
-        console.log(this.id.value)
-        // const id = this.id.value;
+  signUp(callback) {
+    this.submit.addEventListener("click", event => {
+      event.preventDefault();
+      callback();
     })
   }
 }
