b06937ab335f9b76055d3443a7c831622cddcc43
KwonCheulJin <KwonCheulJin@users.noreply.github.com>
2021-04-25 15:45:33 +0900

    signup/login server fetch test

--- frontend/app/signApp.js
+++ frontend/app/signApp.js
@@ -1,4 +1,4 @@
-import SignupView from "../view/view.js/signup.view.js";
+import SignupView from "../view/signup.view.js";
 import SignupController from "../controller/signup.ctrl.js";
 import SignupService from "../service/signup.service.js"
 import User from "../model/user.js";
--- frontend/service/login.service.js
+++ frontend/service/login.service.js
@@ -1,19 +1,21 @@
-// import API from '../api/api.js'
+import API from '../api/api.js'
 
 class LoginService {
   constructor(user) {
     this.user = user
-    // this.api = new API();
+    this.api = new API();
     console.log(this.user)
   }
 
   //로그인 함수
   loginCheckUser(value) {
-    const user = this.user;
-    if (user.id.includes(value.id) && user.pw[user.id.indexOf(value.id)] === value.pw) {
-      console.log("올바른 사용자")
-      this.renderMain();
-    } else console.log("아이디 또는 비밀번호를 확인해주세요.")
+    console.log(value);
+    // const user = this.user;
+    // if (user.id.includes(value.id) && user.pw[user.id.indexOf(value.id)] === value.pw) {
+    //   console.log("올바른 사용자")
+    //   this.renderMain();
+    // } else console.log("아이디 또는 비밀번호를 확인해주세요.")
+    this.api.getLogin(value)
   }
 
   renderMain() {
