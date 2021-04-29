f3fa4830c3fd249a47fd07ac83c6af00cb91977e
Adam <kdh92417@gmail.com>
2021-04-29 18:39:52 +0900

    [Add API] Like Portfolio & Like Company & Search Company API

--- backend/stock_project/account/views.py
+++ backend/stock_project/account/views.py
@@ -3,18 +3,26 @@ import json
 import bcrypt
 
 from django.views       import View
+from django.db.models   import Q
 from django.http        import (
     JsonResponse,
     HttpResponse
 )
 
+
+
 from my_settings         import ALGORITHM
 from stock.settings      import SECRET_KEY
 from .utils              import login_required
 from .models             import Account
-from portfolio.models           import (
+from portfolio.models    import (
     Portfolio,
-    Comment
+    Comment,
+    LikePortfolio,
+)
+from companies.models    import (
+    LikeCompany,
+    Company
 )
 
 
@@ -124,12 +132,14 @@ class UserEmailView(View):
         try:
             modify_data = json.loads(request.body)
             user = Account.objects.get(id=request.user.id)
+            if Account.objects.filter(email=user.email).exists():
+                return JsonResponse({'message': 'This Email has already exists', 'status' : 200}, status=200)
             user.email = modify_data['email']
             user.save()
             return JsonResponse({'message': 'SUCCESS', 'status' : 200}, status=200)
 
-        except KeyError:
-            return JsonResponse({'message': 'KEY_ERROR', 'status' : 400}, status=400)
+        except Exception as e:
+            return JsonResponse({'message': e, 'status' : 400}, status=400)
 
 
 # 이름 변경 API
@@ -156,9 +166,93 @@ class UserPhoneNumberView(View):
             user = Account.objects.get(id=request.user.id)
             user.phone_number = modify_data['phone_number']
             user.save()
-            return JsonResponse({'message': 'SUCCESS', 'status' : 200}, status=200)
+            return JsonResponse({'message': 'success', 'status' : 200}, status=200)
 
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR', 'status' : 400}, status=400)
 
+
+# 생일 변경 API
+class UserBirthDateView(View):
+    @login_required
+    def put(self, request):
+        try:
+            modify_data = json.loads(request.body)
+            user = Account.objects.get(id=request.user.id)
+            user.birth_date = modify_data['birth_date']
+            user.save()
+            return JsonResponse({'message': 'success', 'status' : 200}, status=200)
+
+        except Exception as e:
+            return JsonResponse({'message': e, 'status' : 400}, status=400)
+
+
+class LikePFView(View):
+    @login_required
+    def post(self, request):
+        user = request.user
+        pf_id = json.loads(request.body)['pf_id']
+
+        try:
+            pf = Portfolio.objects.get(id=pf_id)
+            if LikePortfolio.objects.filter(
+                    Q(user_id=user.id) & Q(portfolio_id=pf_id)).exists():
+                user_like_pf = LikePortfolio.objects.filter(
+                    Q(user_id=user.id) & Q(portfolio_id=pf_id)).first()
+
+                pf.total_like -= 1
+                pf.save()
+                user_like_pf.delete()
+
+                return JsonResponse({'message': 'Like this Portfolio', 'status': 200}, status=200)
+            else:
+                try:
+                    LikePortfolio.objects.create(
+                        user_id=user.id,
+                        portfolio_id=pf_id
+                    )
+                    pf.total_like += 1
+                    pf.save()
+                    return JsonResponse({'message': 'Dislike this Portfolio', 'status': 200}, status=200)
+
+                except Exception as e:
+                    return JsonResponse({'message': e, 'status': 400}, status=400)
+
+        except Exception as e:
+            return JsonResponse({'message': e, 'status': 400}, status=400)
+
+
+
+class LikeCPView(View):
+    @login_required
+    def post(self, request):
+        user = request.user
+        cp_name = json.loads(request.body)['cp_name']
+
+        try:
+            cp = Company.objects.get(cp_name=cp_name)
+            if LikeCompany.objects.filter(
+                    Q(user_id=user.id) & Q(company_id=cp.id)).exists():
+                user_like_pf = LikeCompany.objects.filter(
+                    Q(user_id=user.id) & Q(company_id=cp.id)).first()
+                cp.total_like -= 1
+                cp.save()
+                user_like_pf.delete()
+
+                return JsonResponse({'message': 'Like this Company', 'status': 200}, status=200)
+            else:
+                try:
+                    LikeCompany.objects.create(
+                        user_id=user.id,
+                        company_id=cp.id
+                    )
+                    cp.total_like += 1
+                    cp.save()
+                    return JsonResponse({'message': 'Dislike this Company', 'status': 200}, status=200)
+
+                except Exception as e:
+                    return JsonResponse({'message': e, 'status': 400}, status=400)
+
+        except Exception as e:
+            return JsonResponse({'message': e, 'status': 400}, status=400)
 
--- backend/stock_project/companies/views.py
+++ backend/stock_project/companies/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+import json
+from django.views           import View
+from django.http            import (
+    JsonResponse
+)
 
-# Create your views here.
+from companies.models       import Company
+
+
+class SearchCPView(View):
+    def get(self, request):
+        cp_name = request.GET.get('cp_name', None)
+
+        if cp_name != None:
+            try:
+                cp = Company.objects.get(cp_name=cp_name)
+                return JsonResponse({
+                    'message'   : 'success',
+                    'status'    : 200,
+                    'cp_name'   : cp_name,
+                    'corp_code' : cp.corp_code
+                }, status=200)
+            except Company.DoesNotExist:
+                return JsonResponse({
+                    'message' : 'Does Not Exist Company',
+                    'status'  : 400
+                }, status=400)
+        else:
+            return JsonResponse({'message' : 'Key_error', 'status' : 400}, status=400)
