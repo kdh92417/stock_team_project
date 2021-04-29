ce786e712af20db6063be29503a37f7e70913e79
KwonCheulJin <KwonCheulJin@users.noreply.github.com>
2021-04-29 14:39:07 +0900

    modify mypage myinfo MVC

--- frontend/mypage/myinfoApp.js
+++ frontend/mypage/myinfoApp.js
@@ -2,6 +2,5 @@ import MyInfoView from "./myinfo.view.js"
 import MyInfoController from "./myinfo.ctrl.js"
 import MyInfoService from "./myinfo.service.js"
 import API from "../api/api.js"
-import PortfolioView from "../portfolio/portfolio.view.js";
 
-const app = new MyInfoController(new MyInfoService(new API(new MyInfoView())));
+const app = new MyInfoController(new API(new MyInfoView((new MyInfoService))));
