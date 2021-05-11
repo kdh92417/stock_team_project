11ae671509b813a4f6b20653d1a1eef2c3aa583f
KwonCheulJin <KwonCheulJin@users.noreply.github.com>
2021-05-11 17:29:49 +0900

    add mywrite page write and comment delete fuction

--- frontend/mypage/mywrite.service.js
+++ frontend/mypage/mywrite.service.js
@@ -4,6 +4,10 @@ class WriteService {
 
   }
 
+  // functionName - selectAllCheckbox
+  // Job - 작성글 / 작성댓글의 전체삭제를 위한 체크박스 전체선택 함수
+  // Input(args, params) - selectAll, items
+  // Output(return) - none
   selectAllCheckbox = (selectAll, items) => {
     selectAll.addEventListener("click", event => {
       if (event.target.checked === true) {
@@ -12,6 +16,23 @@ class WriteService {
     })
   }
 
+  // functionName - sendDeleteItem
+  // Job - 작성글 / 작성댓글의 전체삭제, 선택삭제, 단일삭제를 위한 함수
+  // Input(args, params) - deleteBtn, items
+  // Output(return) - none
+  sendDeleteItem = (deleteBtn, selectAll, items) => {
+    deleteBtn.addEventListener("click", () => {
+      if (selectAll.checked === true) {
+        console.log("all")
+      } else {
+        for (let i = 0; i < items.length; i++) {
+          if (items[i].checked === true) {
+            console.log(items[i]);
+          }
+        }
+      }
+    })
+  }
 }
 
 export default WriteService;
