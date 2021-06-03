7c801671b59581aa890051d7f4816a5de40e6638
Adam <kdh92417@gmail.com>
2021-06-03 21:31:16 +0900

    [Add] Unit Test - companies app

--- backend/stock_project/companies/tests.py
+++ backend/stock_project/companies/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.test import (
+    TestCase,
+    Client
+)
 
-# Create your tests here.
+from companies.models import (
+    Company,
+    LikeCompany
+)
+
+class SearchCPViewTest(TestCase):
+    def setUp(self):
+        cp = Company.objects.create(
+            cp_name   = 'test_company',
+            corp_code = '1111',
+        )
+
+        cp.count_searching += 1
+        cp.save()
+
+    def tearDown(self):
+        Company.objects.all().delete()
+
+    def test_sarchCPView_get_success(self):
+        client = Client()
+        response = client.get('/company/search/?cp_name=test_company', content_type='application/json')
+        result = {
+            "message"   : "success",
+            "status"    : 200,
+            "cp_name"   : "test_company",
+            "corp_code" : "1111",
+            "like_count": 0
+        }
+
+        self.assertEqual(response.json(), result)
+        self.assertEqual(response.status_code, 200)
+
+
+
